4b2816ad0ddd77a1901825db8bb86dd0d1d08c01
Developer-David <david.rodriguez.form@gmail.com>
2022-05-06 03:21:44 +0300

    try 1 david

---
+++ devdacticNetflix/src/app/tabs/tabs.page.ts
@@ -0,0 +1,20 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss']
+})
+
+export class TabsPage {
+  @ViewChild(IonTabs) tabs;
+  selected = '';
+  
+  constructor() {}
+ 
+  setSelectedTab() {
+    this.selected = this.tabs.getSelected();
+  }
+ 
+}
